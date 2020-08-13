1425aa5368d3f4e78b17ddd7db86689f76833621
Abhishek Sen <abhishek13dgp5@gmail.com>
2020-08-14 02:34:29 +0530

    learn flutter container and basic designing

--- awesome_app/lib/main.dart
+++ awesome_app/lib/main.dart
@@ -6,7 +6,7 @@ void main() {
   // WidgetsApp // MaterialApp // CupertinoApp
   runApp(MaterialApp(
     home: HomePage(),
-    theme: ThemeData(primarySwatch: Colors.green),
+    theme: ThemeData(primarySwatch: Colors.purple),
   ));
 }
 
@@ -17,8 +17,35 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Awesome App"),
       ),
-      body: Container(
-        child: Text("Hi Flutter"),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          // color: Colors.red,  // Removed due to clash with BoxDecoration
+          width: 100,
+          height: 100,
+          clipBehavior: Clip.antiAlias,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+              color: Colors.red,
+              // shape: BoxShape.circle,
+              borderRadius: BorderRadius.circular(10),
+              gradient: LinearGradient(colors: [Colors.pink, Colors.blue]),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  blurRadius: 80,
+                ),
+              ]),
+          child: Text(
+            "I am a Box",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
       ),
     );
   }

2b6e9aed4ea0295f6a4eeb114d9db991452c77cc
Abhishek Sen <abhishek13dgp5@gmail.com>
2020-08-14 20:18:17 +0530

    organise codes & pages

---
+++ awesome_app/lib/drawer.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: const EdgeInsets.all(0),
+        children: <Widget>[
+          UserAccountsDrawerHeader(
+            accountName: Text("Abhishek Sen"),
+            accountEmail: Text("sybergeek@gmail.com"),
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: AssetImage("assets/person.jpg"),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text("Abhishek Sen"),
+            subtitle: Text("Developer"),
+            trailing: Icon(Icons.edit),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.email),
+            title: Text("Email"),
+            subtitle: Text("Developer"),
+            trailing: Icon(Icons.edit),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- awesome_app/lib/main.dart
+++ awesome_app/lib/main.dart
@@ -1,111 +1,17 @@
-import 'package:flutter/widgets.dart';
+import 'package:awesome_app/pages/home_page.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
 
 void main() {
   // WidgetsApp // MaterialApp // CupertinoApp
-  runApp(MaterialApp(
-    home: HomePage(),
-    theme: ThemeData(primarySwatch: Colors.purple),
-  ));
+  runApp(MyApp());
 }
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  var myText = "Change My Name";
-  TextEditingController _nameController = TextEditingController();
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: Text("Awesome App"),
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: SingleChildScrollView(
-            child: Card(
-              child: Column(
-                children: <Widget>[
-                  Image.asset(
-                    "assets/coding.jpg",
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    myText,
-                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: TextField(
-                      controller: _nameController,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: "Enter some text",
-                        labelText: "Name",
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: const EdgeInsets.all(0),
-          children: <Widget>[
-            UserAccountsDrawerHeader(
-              accountName: Text("Abhishek Sen"),
-              accountEmail: Text("sybergeek@gmail.com"),
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: AssetImage("assets/person.jpg"),
-              ),
-            ),
-            ListTile(
-              leading: Icon(Icons.person),
-              title: Text("Abhishek Sen"),
-              subtitle: Text("Developer"),
-              trailing: Icon(Icons.edit),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: Icon(Icons.email),
-              title: Text("Email"),
-              subtitle: Text("Developer"),
-              trailing: Icon(Icons.edit),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          myText = _nameController.text;
-          setState(() {});
-        },
-        child: Icon(Icons.send),
-      ),
+    return MaterialApp(
+      home: HomePage(),
+      theme: ThemeData(primarySwatch: Colors.purple),
     );
   }
 }
